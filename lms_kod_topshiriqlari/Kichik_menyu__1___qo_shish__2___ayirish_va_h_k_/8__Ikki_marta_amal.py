397a296f47e450859bc1f32a44051ac943b67c51
Elmurodov Sardor <sardor007elmurodov@gmail.com>
2026-05-28 18:39:00 +0500

    Add: 8) Ikki marta amal [sardor007elmurodov-cpu]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/8__Ikki_marta_amal.py
@@ -0,0 +1,14 @@
+
+for i in range(2):
+    a, b = map(int, input().split())
+    n = int(input())
+    if n == 1:
+        print(a + b)
+      
+    elif n == 2:
+        print(a - b)
+n = int(input())
+if n == 0:
+    print("Exit")
+        
+        
